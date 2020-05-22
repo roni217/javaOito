f10ab634904c7f85085af12365b9c3fc45a97122
roni <roni.217@gmail.com>
2020-05-22 18:56:52 -0300

    Syso e .sort com lambdas

--- src/javaOito/OrdenaStrings.java
+++ src/javaOito/OrdenaStrings.java
@@ -4,24 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
 	public static void main(String[] args) {
 		List<String> palavras = new ArrayList<String>();
 		populaListaPalavras(palavras);
-		
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		
-		palavras.sort(comparador); 
 		System.out.println(palavras);
-
-		palavras.forEach(new Consumer<String>() {
-			@Override
-			public void accept(String s) {
-				System.out.println(s);	
-			}
+		
+		palavras.sort((s1, s2) -> {
+		    return Integer.compare(s1.length(), s2.length());
 		});
+
+		palavras.forEach((String s) -> System.out.println(s));
 		
 	}
 
@@ -40,16 +34,3 @@ public class OrdenaStrings {
 		palavras.add("palavra04");
 	}
 }
-
-class ComparadorPorTamanho implements Comparator<String>{
-
-	@Override
-	public int compare(String o1, String o2) {
-		if(o1.length() < o2.length())
-			return -1;
-		if(o1.length() > o2.length())
-			return 1;
-		return 0;
-	}
-}
-
