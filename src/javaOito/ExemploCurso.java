bb56363dfb23f645fc788bf865676bca27d2a5b9
roni <roni.217@gmail.com>
2020-05-25 19:56:20 -0300

    Mais Streams Collectors e APIs

--- src/javaOito/ExemploCurso.java
+++ src/javaOito/ExemploCurso.java
@@ -3,6 +3,9 @@ package javaOito;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ExemploCurso {
 	public static void main(String[] args) {
@@ -13,23 +16,39 @@ public class ExemploCurso {
 		cursos.add(new Curso("C", 55));
 
 		cursos.sort(Comparator.comparing(Curso::getQtdAlunos));
-
 		cursos.forEach(c -> System.out.println(c.getNome()));
-
 		cursos.stream().filter(c -> c.getQtdAlunos() >= 100);
+
 		System.out.println("\n\n");
+
 		cursos.stream().filter(c -> c.getQtdAlunos() > 100).forEach(System.out::println);
-		
-		cursos.stream()
-		   .filter(d -> d.getQtdAlunos() > 100)
-		   .map(Curso::getQtdAlunos)
-		   .forEach(x -> System.out.println(x));
-		
-		int soma = cursos.stream()
-				   .filter(c -> c.getQtdAlunos() > 100)
-				   .mapToInt(Curso::getQtdAlunos)
-				   .sum();
-		System.out.println("Total de alunos: " +soma);
+
+		System.out.println("\n\n");
+
+		cursos.stream().filter(d -> d.getQtdAlunos() > 100).map(Curso::getQtdAlunos)
+				.forEach(x -> System.out.println(x));
+
+		int soma = cursos.stream().filter(c -> c.getQtdAlunos() > 100).mapToInt(Curso::getQtdAlunos).sum();
+
+		System.out.println("Total de alunos: " + soma);
+
+		Optional<Curso> optCurso = cursos.stream().filter(c -> c.getQtdAlunos() > 100).findAny();
+
+		Curso curso = optCurso.orElse(null);
+		System.out.println(curso.getNome());
+
+		optCurso.ifPresent(c -> System.out.println(c.getNome()));
+
+		// com map
+		Map<String, Integer> mapa = cursos.stream().filter(c -> c.getQtdAlunos() > 100)
+				.collect(Collectors.toMap(c -> c.getNome(), c -> c.getQtdAlunos()));
+		System.out.println(mapa);
+
+		// ou
+		cursos.stream().filter(c -> c.getQtdAlunos() > 100)
+				.collect(Collectors.toMap(c -> c.getNome(), c -> c.getQtdAlunos()))
+				.forEach((nome, alunos) ->  System.out.println(nome + " tem " + alunos));
+
 	}
 }
 
@@ -49,10 +68,10 @@ class Curso {
 	public int getQtdAlunos() {
 		return qtdAlunos;
 	}
-	
+
 	@Override
 	public String toString() {
-		String ret ="Nome: "+this.nome+"\nQuantidade De Alunos "+this.qtdAlunos; 
+		String ret = "Nome: " + this.nome + "\nQuantidade De Alunos " + this.qtdAlunos;
 		return ret;
 	}
 
